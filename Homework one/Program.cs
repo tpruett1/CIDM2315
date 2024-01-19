867ecb78dad4a05c1b9df3ec9732636cb19c6f78
tpruett1 <156725093+tpruett1@users.noreply.github.com>
2024-01-19 15:18:21 -0600

    Add files via upload

---
+++ Homework one/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Homeworkone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 2.5;
+            double y = 3.3;
+            double z = 4 * x * x + 3 * y;
+
+            Console.WriteLine("X = " + x + ", Y = " + y);
+            Console.WriteLine("Z = " + z);
+        }
+    }
+}
