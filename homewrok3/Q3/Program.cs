ab518837907b6f1aa99cca8a5c9a68c33866a2c9
tpruett1 <156725093+tpruett1@users.noreply.github.com>
2024-02-01 15:21:09 -0600

    Add files via upload

---
+++ homewrok3/Q3/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int lines = 1;
+            int colums = 0;
+            var clear = false;
+            Console.WriteLine("Assign a value to N:");
+            n = Convert.ToInt16(Console.ReadLine());
+            while(n>=lines)
+            {
+
+                while(lines>colums)
+                {
+                    Console.Write("*");
+                    colums++;
+
+                    if(lines==colums)
+                    {
+                        clear=true;
+                    }
+                }
+
+                lines++;
+                Console.WriteLine();
+                if(clear)
+                {
+                    colums=0;
+                }
+                
+                
+                
+            }
+
+
+        }
+    }
+}
